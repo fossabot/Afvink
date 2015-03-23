98f76d6b6dd9b7d32e9ffd86183a7231347ba20c
Joulle <joel.tewierik@gmail.com>
2015-03-23 12:26:00 +0100

    modified:   jemoederdikke1.py

--- jemoederdikke1.py
+++ jemoederdikke1.py
@@ -1,2 +1,20 @@
-#Hello world
-print ("Hello world")
+def main():
+    regels = ""
+    teller = 0
+    lengte = 0.0
+
+    bestand =  'atgcatgctagctagcctagctagcatcgta' #open('README.md')
+    for line in bestand:
+        regels = regels + line
+        
+    for letter in regels:
+        print letter
+        if letter == "g" or letter == "c":
+            teller += 1
+        if letter == "g" or letter == "c" or letter == "a" or letter == "t":
+            lengte += 1
+    
+    gcperc =  (teller/lengte)*100
+    
+    print "het GC-percentage is" , gcperc , "%. "  
+main()
